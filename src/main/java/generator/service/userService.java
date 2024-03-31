fd92c1183d76a493896d3dc07366554df2ed718f
黄瑜峰 <156048846@qq.com>
2024-03-31 15:33:55 +0800

    第一次提交

---
+++ src/main/java/generator/service/userService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.user;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Hasee
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2024-03-30 20:44:57
+*/
+public interface userService extends IService<user> {
+
+}
